45238012e732435a2ebe2ff5cd652421e5ae2790
lexisbr <josebarriosrodas@gmail.com>
2020-08-22 20:07:42 -0600

    Trabajando en visualizar

--- Proyecto1/src/main/java/interfaces/MainEmpresa.java
+++ Proyecto1/src/main/java/interfaces/MainEmpresa.java
@@ -161,7 +161,9 @@ public class MainEmpresa extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+       SeleccionarVisualizar a = new SeleccionarVisualizar();
+       a.setVisible(true);
+       this.dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
